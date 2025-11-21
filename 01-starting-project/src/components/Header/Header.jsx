d2cbced7bcb6af82446c93affbf23c70f1fd6cd6
Emma Persephone Purvis <pklemma777@gmail.com>
2025-11-21 13:57:09 +1100

    Starting project

---
+++ 01-starting-project/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import stylizedAtom from "../../assets/react-core-concepts.png";
+import "./Header.css";
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  // dynamic element 
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
+  return (
+    <header>
+      <img src={stylizedAtom} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
