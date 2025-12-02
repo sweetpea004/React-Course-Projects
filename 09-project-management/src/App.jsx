177a75cba4a83c7e5a27e5a67b9e775dd8f93198
Emma Persephone Purvis <pklemma777@gmail.com>
2025-12-03 01:06:14 +1100

    progress made on project management app

--- 09-project-management/src/App.jsx
+++ 09-project-management/src/App.jsx
@@ -1,8 +1,76 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProject: undefined,
+    projects: []
+  });
+
+  function handleSelectProject(id) {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: id
+      }
+    });
+  }
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: null
+      }
+    });
+  }
+
+  function handleCancelAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: undefined
+      }
+    });
+  }
+
+  function handleAddProject(projectData) {
+    const newProject = {
+      ...projectData,
+      id: Math.random()
+    };
+
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProject)
+
+  let content = <SelectedProject project={selectedProject}/>;
+
+  if (projectState.selectedProject === null) {
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
+  } else if (projectState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar 
+        onStartAddProject={handleStartAddProject} 
+        projects={projectState.projects}
+        onSelectProject={handleSelectProject}
+      />
+      {content}
+    </main>
   );
 }
 
---
+++ 09-project-management/src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import { createPortal } from "react-dom";
+import { useImperativeHandle, useRef } from "react";
+import Button from "./Button";
+
+export default function Modal({children, ref, buttonCaption}) {
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        };
+    });
+
+    return createPortal(
+        <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
+            {children}
+            <form method="dialog" className="mt-3 text-right"> 
+                <Button>{buttonCaption}</Button>
+            </form>
+        </dialog>,
+        document.getElementById("modal-root")
+    );
+}
