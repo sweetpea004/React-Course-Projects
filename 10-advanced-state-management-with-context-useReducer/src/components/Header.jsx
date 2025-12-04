235175d996c28a8652e2cad7afeb53c258415ffe
Emma Persephone Purvis <pklemma777@gmail.com>
2025-12-04 22:48:22 +1100

    finished context and useReducer project

--- 10-advanced-state-management-with-context-useReducer/src/components/Header.jsx
+++ 10-advanced-state-management-with-context-useReducer/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { useRef } from 'react';
+import { useRef, use } from 'react';
 
+import { CartContext } from '../store/shopping-cart-context.jsx';
 import CartModal from './CartModal.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header({ }) {
   const modal = useRef();
+  const { items } = use(CartContext);
 
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
@@ -26,8 +28,6 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
     <>
       <CartModal
         ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
         title="Your Cart"
         actions={modalActions}
       />
