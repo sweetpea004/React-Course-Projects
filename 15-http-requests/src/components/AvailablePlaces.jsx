7d14d2763801ef6f0564e3404cad8a8ca5aed0bb
Emma Persephone Purvis <pklemma777@gmail.com>
2025-12-09 16:46:32 +1100

    progress on htttp project

--- 15-http-requests/src/components/AvailablePlaces.jsx
+++ 15-http-requests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    setIsFetching(true);
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching Place Data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
