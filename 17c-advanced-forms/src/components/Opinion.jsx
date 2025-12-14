b43afc3113fd50bfa4f8246e863fd190245a75b1
Emma Persephone Purvis <pklemma777@gmail.com>
2025-12-14 11:29:20 +1100

    completed 17c advanced forms

--- 17c-advanced-forms/src/components/Opinion.jsx
+++ 17c-advanced-forms/src/components/Opinion.jsx
@@ -1,4 +1,26 @@
+import { use, useActionState, useOptimistic } from "react";
+import { OpinionsContext } from "../store/opinions-context";
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+  const {upvoteOpinion, downvoteOpinion} = use(OpinionsContext)
+
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(votes, (prevVotes, mode) => 
+    mode === 'up' ? prevVotes + 1 : prevVotes - 1
+  );
+  
+  async function upvoteAction() {
+    setVotesOptimistically('up');
+    await upvoteOpinion(id);
+  }
+
+  async function downvoteAction() {
+    setVotesOptimistically('down');
+    await downvoteOpinion(id);
+  }
+
+  const [upvoteFormState, upvoteFormAction, upvotePending] = useActionState(upvoteAction);
+  const [downvoteFormState, downvoteFormAction, downVotePending] = useActionState(downvoteAction);
+  
   return (
     <article>
       <header>
@@ -7,7 +29,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button>
+        <button formAction={upvoteFormAction} disabled={upvotePending || downVotePending}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -25,9 +47,9 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
-        <button>
+        <button formAction={downvoteFormAction} disabled={upvotePending || downVotePending}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
