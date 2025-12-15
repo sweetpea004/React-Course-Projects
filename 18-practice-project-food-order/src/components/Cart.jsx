432798bcb242aa4c933fa930989d131b363479b0
Emma Persephone Purvis <pklemma777@gmail.com>
2025-12-15 20:56:28 +1100

    finished practice project food app and added new project redux basics

---
+++ 18-practice-project-food-order/src/components/Cart.jsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContent";
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
+
+export default function Cart() {
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+
+    const cartTotal = cartCtx.items.reduce(
+        (totalPrice, item) => totalPrice + item.quantity * item.price, 
+        0
+    );
+
+    function handleCloseCart() {
+        userProgressCtx.hideCart();
+    }
+
+    function handleOpenCheckout() {
+        userProgressCtx.showCheckout();
+    }
+
+    return (
+        <Modal 
+            className="cart" 
+            onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null} 
+            open={userProgressCtx.progress === 'cart'}
+        >
+            <h2>Your Cart</h2>
+            <ul>
+                {cartCtx.items.map((item) => 
+                    <CartItem 
+                        key={item.id} 
+                        {...item} 
+                        onIncrease={() => cartCtx.addItem(item)} 
+                        onDecrease={() => cartCtx.removeItem(item.id)}/>
+                )}
+            </ul>
+            <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+            <p className="modal-actions">
+                <Button textOnly onClick={handleCloseCart}>Close</Button>
+                {cartCtx.items.length > 0 && 
+                    <Button onClick={handleOpenCheckout}>Go to Checkout</Button>
+                }
+            </p>
+        </Modal>
+    );
+}
---
+++ 18-practice-project-food-order/src/components/Meals.jsx
@@ -0,0 +1,33 @@
+import Meal from "./Meal";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig = {}
+
+export default function Meals() {
+    const {
+        data: loadedMeals, 
+        isLoading, 
+        error
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+    if (isLoading) {
+        return <p className="center">Fetching meals...</p>
+    }
+
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error}/>
+    }
+
+    // if (!data) {
+    //     return <p>No Meals Found.</p>
+    // }
+
+    return (
+        <ul id="meals">
+            {loadedMeals.map((meal) => 
+                <Meal key={meal.id} meal={meal}/>
+            )}
+        </ul>
+    );
+}
