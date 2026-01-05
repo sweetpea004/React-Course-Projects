4ff1f3e7e2d7a2966b78def5299533bb7366b686
Emma Persephone Purvis <pklemma777@gmail.com>
2026-01-05 20:19:40 +1100

    progress on nextjs project b

---
+++ 26b-NextJS/app/meals/not_found.js
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+    return (
+        <main>
+            <h1>Meal not found</h1>
+            <p>Unfortunately, we could not find the requested page or meal data.</p>
+        </main>
+    )
+}
---
+++ 26b-NextJS/app/not-found.js
@@ -0,0 +1,7 @@
+
+export default function NotFound({ error }) {
+    return <main className="not-found">
+       <h1>Not found</h1>
+       <p>Unfortunately, we could not find the requested page or resource.</p> 
+    </main>
+}
