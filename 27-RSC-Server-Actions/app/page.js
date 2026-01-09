69eabc9532d6eacd080b68b3778b2efccce1c686
Emma Persephone Purvis <pklemma777@gmail.com>
2026-01-09 13:35:27 +1100

    Added new Project 27

---
+++ 27-RSC-Server-Actions/app/page.js
@@ -0,0 +1,11 @@
+import ClientDemo from "@/components/ClientDemo";
+import RSCDemo from "@/components/RSCDemo";
+
+export default function Home() {
+  return (
+    <main>
+      <RSCDemo />
+      <ClientDemo />
+    </main>
+  );
+}
