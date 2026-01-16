8b0aa2dafeff25d671e3d0168f8a55abfbc1420e
Emma Persephone Purvis <pklemma777@gmail.com>
2026-01-16 11:38:16 +1100

    completed project 31, testing react apps

---
+++ 31-testing-react-apps/src/components/Async.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+
+describe('Async Component', () => {
+    test('Renders posts if request succeeds', async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 'p1', title: 'first post'}],
+        });
+        
+        // Arrange
+        render(<Async />);
+
+        // Assert
+        // find used for elements that dont render immediately (eg. fetch or async) 
+        // get used for elements immediately rendered
+        const listItemElements = await screen.findAllByRole('listitem');
+        expect(listItemElements).not.toHaveLength(0);
+    });
+})
---
+++ 31-testing-react-apps/src/components/Greeting.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+import Output from "./Output";
+
+const Greeting = () => {
+    const [changedText, setChangedText] = useState(false);
+
+    const changedTextHandler = () => {
+        setChangedText(true);
+    };
+
+    return (
+        <div>
+            <h2>Hello World!</h2>
+            {changedText ? <Output>Changed</Output> : <Output>It's good to see you!</Output>}
+            <button onClick={changedTextHandler}>Change Text!</button>
+        </div>
+    );
+};
+
+export default Greeting;
