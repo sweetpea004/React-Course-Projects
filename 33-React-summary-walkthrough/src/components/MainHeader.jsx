e2a04cfa675cd161c342b658169fd0e8c7b86fcc
Emma Persephone Purvis <pklemma777@gmail.com>
2026-01-20 17:19:31 +1100

    completed project 33 react summary

--- 33-React-summary-walkthrough/src/components/MainHeader.jsx
+++ 33-React-summary-walkthrough/src/components/MainHeader.jsx
@@ -1,8 +1,9 @@
 import { MdPostAdd, MdMessage } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 import classes from './MainHeader.module.css';
 
-function MainHeader({ onCreatePost }) {
+function MainHeader() {
   
 
   return (
@@ -12,10 +13,10 @@ function MainHeader({ onCreatePost }) {
         React Poster
       </h1>
       <p>
-        <button className={classes.button} onClick={onCreatePost}>
+        <Link to='/create-post' className={classes.button}>
           <MdPostAdd size={18} />
           New Post
-        </button>
+        </Link>
       </p>
     </header>
   );
--- 33-React-summary-walkthrough/src/components/PostList.jsx
+++ 33-React-summary-walkthrough/src/components/PostList.jsx
@@ -1,32 +1,28 @@
-import { useState } from "react";
-import NewPost from "./NewPost";
-import Post from "./Post";
-import Modal from "./Modal";
-import classes from './PostList.module.css'
+import { useLoaderData } from 'react-router-dom';
 
-function PostList({isPosting, onStopPosting}) {
-    const [posts, setPosts] = useState([]);
+import Post from './Post';
+import classes from './PostList.module.css';
 
-    function addPostHandler(postData) {
-        setPosts((exisiting) => [postData, ...exisiting]);
-    }
+function PostList() {
+  const posts = useLoaderData();
 
-    return (
-        <>
-            {isPosting ? 
-                <Modal onClose={onStopPosting}>
-                    <NewPost 
-                        onCancel={onStopPosting}
-                        onAddPost={addPostHandler}
-                    />
-                </Modal> : false
-            }
-            
-            <ul className={classes.posts}>
-                
-            </ul>
-        </>
-    );
+  return (
+    <>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.id} id={post.id} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default PostList;
